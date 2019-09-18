f30ac8985d0ca7e20eae08d7a68689822d656324
maxwell-oroark <m.d.oroark@gmail.com>
2019-09-18 12:08:07 -0600

    playing around with strava api

--- src/App.js
+++ src/App.js
@@ -2,25 +2,44 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  componentDidMount(){
+    navigator.geolocation.getCurrentPosition(position => {
+      console.log('position:', position) 
+    }) 
+  }
+
+  connectStrava = () => {
+    const clientId = `39086`;
+    const redirectUri = `http://localhost:3000/token`;
+    const responseType = `code`;
+    const scope = `read`;
+    window.location = `https://www.strava.com/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}&scope=${scope}`
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <button onClick={this.connectStrava} >Connect Strava Account</button>
+        </header>
+      </div>
+    );
+  
+  }
 }
 
 export default App;

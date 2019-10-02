733a6328444067045f286b351bf5365f7e1d2809
maxwell-oroark <m.d.oroark@gmail.com>
2019-10-02 16:38:56 -0500

    have code coming back from strava

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import TokenHandler from './TokenHandler';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,7 +8,7 @@ class App extends React.Component {
 
   componentDidMount(){
     navigator.geolocation.getCurrentPosition(position => {
-      console.log('position:', position) 
+      console.log('users position:', position) 
     }) 
   }
 
@@ -14,29 +16,37 @@ class App extends React.Component {
     const clientId = `39086`;
     const redirectUri = `http://localhost:3000/token`;
     const responseType = `code`;
-    const scope = `read`;
-    window.location = `https://www.strava.com/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}&scope=${scope}`
+    window.location = `https://www.strava.com/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}&scope=read`
   }
 
   render(){
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <button onClick={this.connectStrava} >Connect Strava Account</button>
-        </header>
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <div className="App">
+              <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                  Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                  className="App-link"
+                  href="https://reactjs.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Learn React
+                </a>
+                <button onClick={this.connectStrava} >Connect Strava Account</button>
+              </header>
+            </div>
+          </Route>
+          <Route exact path='/token'>
+            <TokenHandler />
+          </Route>
+        </Switch>
+      </Router>
     );
   
   }

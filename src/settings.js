9da7ee81f8ab342bb73d4978b6ddb752fe9ffac0
maxwell-oroark <m.d.oroark@gmail.com>
2019-10-05 18:56:35 -0500

    add wwww

--- src/settings.js
+++ src/settings.js
@@ -10,7 +10,7 @@ const settings = (env) => {
       MAPBOX_TOKEN:'pk.eyJ1IjoibWF4d2VsbG8iLCJhIjoiY2ltc2Fma2UxMDFpb3ZsbTR2MjJvMmlwcCJ9.-czd-gCxko0qiczeOvUbig',
       AWS_BACKEND: 'https://cxwj5fkd00.execute-api.us-east-1.amazonaws.com/prod',
       STRAVA_CLIENT_ID: '39086',
-      STRAVA_REDIRECT_URI: 'lucid-mcnulty-6d1630.netlify.com/token'
+      STRAVA_REDIRECT_URI: 'www.lucid-mcnulty-6d1630.netlify.com/token'
     }
   }
   return config[env]

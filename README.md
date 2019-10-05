bb23937f9a38ff0c0ac10070c09fcc09e1e67b7e
maxwell-oroark <m.d.oroark@gmail.com>
2019-10-05 18:39:10 -0500

    add documenation plus env vars for prod

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 ## What is KOM killer?
 
 An application for helping you find easy KOMs near your live location. Simply authenticate with strava and 
-navigate to /koms and you can see a detailed breakdown of segments that are most acheivable given your FTP.
+navigate to `/koms` and you can see a detailed breakdown of segments that are most acheivable given your FTP.
+
+## Where is KOM Killer?
+
+[https://lucid-mcnulty-6d1630.netlify.com](https://lucid-mcnulty-6d1630.netlify.com)
+
--- src/settings.js
+++ src/settings.js
@@ -1,8 +1,19 @@
-const config = {
-  MAPBOX_TOKEN:'pk.eyJ1IjoibWF4d2VsbG8iLCJhIjoiY2ltc2Fma2UxMDFpb3ZsbTR2MjJvMmlwcCJ9.-czd-gCxko0qiczeOvUbig',
-  AWS_BACKEND: 'https://cxwj5fkd00.execute-api.us-east-1.amazonaws.com/prod',
-  STRAVA_CLIENT_ID: '39086',
-  STRAVA_REDIRECT_URI: 'http://localhost:3000/token'
+const settings = (env) => {
+  const config = {
+    development: {
+      MAPBOX_TOKEN:'pk.eyJ1IjoibWF4d2VsbG8iLCJhIjoiY2ltc2Fma2UxMDFpb3ZsbTR2MjJvMmlwcCJ9.-czd-gCxko0qiczeOvUbig',
+      AWS_BACKEND: 'https://cxwj5fkd00.execute-api.us-east-1.amazonaws.com/prod',
+      STRAVA_CLIENT_ID: '39086',
+      STRAVA_REDIRECT_URI: 'http://localhost:3000/token'
+    },
+    production: {
+      MAPBOX_TOKEN:'pk.eyJ1IjoibWF4d2VsbG8iLCJhIjoiY2ltc2Fma2UxMDFpb3ZsbTR2MjJvMmlwcCJ9.-czd-gCxko0qiczeOvUbig',
+      AWS_BACKEND: 'https://cxwj5fkd00.execute-api.us-east-1.amazonaws.com/prod',
+      STRAVA_CLIENT_ID: '39086',
+      STRAVA_REDIRECT_URI: 'lucid-mcnulty-6d1630.netlify.com/token'
+    }
+  }
+  return config[env]
 }
 
-export default config;
+export default settings(process.ENV.REACT_APP_ENV)
